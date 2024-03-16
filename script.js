99db53d0981027134d08abcf4bffa36d1fe54d42
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-03-16 22:32:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-//complete this code
-class Person {}
+// Person class
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+}
 
-// Do not change the code below this line
+// Student subclass extending Person
+class Student extends Person {
+  // Method for studying
+  study() {
+    console.log(`${this._name} is studying`);
+  }
+}
+
+// Teacher subclass extending Person
+class Teacher extends Person {
+  // Method for teaching
+  teach() {
+    console.log(`${this._name} is teaching`);
+  }
+}
+
+// Export the classes
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
